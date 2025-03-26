a68cf559beb5f18f44825aa1f943edb81519f711
troy96475 <taesupark0504@gmail.com>
2025-03-26 09:42:04 +0900

    feat :: 간단한 피드 도메인

---
+++ src/main/java/com/example/s3_practice/domain/feed/domain/Feed.java
@@ -0,0 +1,24 @@
+package com.example.s3_practice.domain.feed.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+}
