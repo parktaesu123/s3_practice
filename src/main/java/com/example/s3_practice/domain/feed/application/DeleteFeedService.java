b526364fa959641b09e68aaa0d60cd70bcbddee6
troy96475 <taesupark0504@gmail.com>
2025-03-26 14:59:20 +0900

    refactor :: 삭제 서비스

--- src/main/java/com/example/s3_practice/domain/feed/application/DeleteFeedService.java
+++ src/main/java/com/example/s3_practice/domain/feed/application/DeleteFeedService.java
@@ -3,20 +3,33 @@ package com.example.s3_practice.domain.feed.application;
 import com.example.s3_practice.domain.feed.domain.Feed;
 import com.example.s3_practice.domain.feed.domain.repository.FeedRepository;
 import com.example.s3_practice.domain.feed.exception.FeedNotFoundException;
+import com.example.s3_practice.infra.s3.service.S3AsyncService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 public class DeleteFeedService {
     private final FeedRepository feedRepository;
+    private final S3AsyncService s3AsyncService;
 
     @Transactional
-    public void delete(Long id) {
+    public CompletableFuture<Void> delete(Long id, String imageUrl) {
         Feed feed = feedRepository.findById(id)
                         .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
 
-        feedRepository.delete(feed);
+        CompletableFuture<Void> imageDeleteFuture = null;
+        if (imageUrl != null && !imageUrl.isEmpty()) {
+            imageDeleteFuture = s3AsyncService.deleteAsync(imageUrl);
+        } else {
+            imageDeleteFuture = CompletableFuture.completedFuture(null);
+        }
+
+        return imageDeleteFuture.thenRunAsync(() -> {
+            feedRepository.delete(feed);
+        });
     }
 }
